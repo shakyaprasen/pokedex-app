77357ac61d332977be4dfd3c16f82d5901219649
Prasen Shakya <prasen.construct@gmail.com>
2018-11-22 19:07:24 +0545

    removed /

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.use(express.static(__dirname + '/dist/pokedex-app'));
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist//pokedex-app/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/pokedex-app/index.html'));
 });
